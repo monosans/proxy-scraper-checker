2aaba95050362524e2b7d00ab71d012b07f72c2d
monosans <hsyqixco@protonmail.com>
2022-11-11 23:27:21 +0300

    Update user agent

--- main.py
+++ main.py
@@ -260,8 +260,8 @@ class ProxyScraperChecker:
         }
         headers = {
             "User-Agent": (
-                "Mozilla/5.0 (Windows NT 10.0; rv:105.0)"
-                + " Gecko/20100101 Firefox/105.0"
+                "Mozilla/5.0 (Windows NT 10.0; rv:106.0)"
+                + " Gecko/20100101 Firefox/106.0"
             )
         }
         async with ClientSession(headers=headers) as session:

2bd38fdcd18b54dba477e14fd3a3a22319b52949
monosans <hsyqixco@protonmail.com>
2023-04-12 15:40:01 +0300

    Update User-Agent

--- proxy_scraper_checker/constants.py
+++ proxy_scraper_checker/constants.py
@@ -1,3 +1,3 @@
 from __future__ import annotations
 
-USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; rv:109.0) Gecko/20100101 Firefox/109.0"
+USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; rv:111.0) Gecko/20100101 Firefox/111.0"

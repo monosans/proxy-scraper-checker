28b751ee808c9c56676da6a944a6b20dba1734e2
monosans <hsyqixco@protonmail.com>
2023-05-19 23:51:20 +0300

    Reformat start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-python3 -m pip install -U --no-cache-dir --disable-pip-version-check pip setuptools wheel && python3 -m pip install -U --no-cache-dir --disable-pip-version-check -r requirements.txt && python3 -m proxy_scraper_checker
+python3 -m pip install -U --no-cache-dir --disable-pip-version-check pip setuptools wheel &&
+python3 -m pip install -U --no-cache-dir --disable-pip-version-check -r requirements.txt &&
+python3 -m proxy_scraper_checker

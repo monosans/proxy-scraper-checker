89dd77aaed1b576ac5f996b8b81d01eb31b847f2
monosans <hsyqixco@protonmail.com>
2023-07-12 23:37:40 +0300

    Fix Termux

--- install-termux.sh
+++ install-termux.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
+path=~/proxy-scraper-checker
 pkg upgrade --yes -o Dpkg::Options::='--force-confdef' &&
 pkg install --yes python python-pip &&
-rm -rfi ~/proxy-scraper-checker &&
-curl -fsSL 'https://github.com/monosans/proxy-scraper-checker/archive/refs/heads/main.zip' | unzip - -d ~/proxy-scraper-checker &&
+if [ -d "${path}" ]; then
+    rm -rfi "${path}"
+fi &&
+curl -fsSL 'https://github.com/monosans/proxy-scraper-checker/archive/refs/heads/main.zip' | unzip - -d "${path}" &&
 python3 -m pip install -U --no-cache-dir --disable-pip-version-check setuptools wheel &&
-python3 -m pip install -U --no-cache-dir --disable-pip-version-check -r ~/proxy-scraper-checker/requirements.txt &&
-echo "proxy-scraper-checker installed successfully.\nRun 'cd ~/proxy-scraper-checker && sh start-termux.sh'."
+python3 -m pip install -U --no-cache-dir --disable-pip-version-check -r "${path}"/requirements.txt &&
+printf "proxy-scraper-checker installed successfully.\nRun 'cd %s && sh start-termux.sh'." "${path}"

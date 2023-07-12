173c7946207f2b11dfcedaa30c306c7db09e3a13
monosans <hsyqixco@protonmail.com>
2023-07-12 23:55:28 +0300

    Fix Termux

--- install-termux.sh
+++ install-termux.sh
@@ -10,5 +10,5 @@ fi &&
 curl -fsSLo "${download_path}" 'https://github.com/monosans/proxy-scraper-checker/archive/refs/heads/main.zip' &&
 unzip -jd "${path}" "${download_path}" &&
 python3 -m pip install -U --no-cache-dir --disable-pip-version-check setuptools wheel &&
-python3 -m pip install -U --no-cache-dir --disable-pip-version-check -r "${path}"/requirements.txt &&
+python3 -m pip install -U --no-cache-dir --disable-pip-version-check -r "${path}"/requirements-termux.txt &&
 printf "proxy-scraper-checker installed successfully.\nRun 'cd %s && sh start-termux.sh'." "${path}"

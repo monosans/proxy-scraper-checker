1a8d3e00423321d4a53542344c7bda0907e84456
Victor is Geek <92975790+victorgeel@users.noreply.github.com>
2023-11-09 01:59:18 +0630

    Update start-termux.sh

--- start-termux.sh
+++ start-termux.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 python3 -m pip install -U --no-cache-dir --disable-pip-version-check setuptools wheel &&
 python3 -m pip install -U --no-cache-dir --disable-pip-version-check -r requirements-termux.txt &&
-python3 -m proxy-here-Noobs
+python3 -m proxy_scraper_checker

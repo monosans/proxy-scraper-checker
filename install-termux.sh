fbe220b0f5de7191a02dc6e97242bb001900fd71
Victor is Geek <92975790+victorgeel@users.noreply.github.com>
2023-11-09 02:05:45 +0630

    Update install-termux.sh

--- install-termux.sh
+++ install-termux.sh
@@ -10,7 +10,7 @@ if [ -d "${path}" ]; then
 fi &&
 curl -fsSLo "${download_path}" 'https://github.com/victorgeel/proxy-here-Noobs/archive/refs/heads/modified.zip' &&
 unzip -d "${base_path}" "${download_path}" &&
-mv "${path}-main" "${path}" &&
+mv "${path}-modified" "${path}" &&
 python3 -m pip install -U --no-cache-dir --disable-pip-version-check setuptools wheel &&
 python3 -m pip install -U --no-cache-dir --disable-pip-version-check -r "${path}/requirements-termux.txt" &&
 printf "proxy-here-Noobs installed successfully.\nRun 'cd %s && sh start-termux.sh'.\n" "${path}"

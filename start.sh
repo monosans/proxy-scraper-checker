5164a764e6d465961ebd173b26705a70b38f6d36
Victor is Geek <92975790+victorgeel@users.noreply.github.com>
2023-11-08 19:05:49 +0630

    Add files via upload

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-python3 -m pip install -U --no-cache-dir --disable-pip-version-check pip setuptools wheel &&
-python3 -m pip install -U --no-cache-dir --disable-pip-version-check -r requirements.txt &&
-python3 -m proxy_scraper_checker
+python -m pip install -U --no-cache-dir --disable-pip-version-check pip setuptools wheel &&
+python -m pip install -U --no-cache-dir --disable-pip-version-check -r requirements.txt &&
+python -m proxy_scraper_checker

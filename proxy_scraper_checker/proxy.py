fb59bdaf70921ea71644fe1b8247c92146f71a01
monosans <hsyqixco@protonmail.com>
2023-11-24 19:54:03 +0300

    Update User-Agent

--- proxy_scraper_checker/proxy.py
+++ proxy_scraper_checker/proxy.py
@@ -16,8 +16,8 @@ DEFAULT_CHECK_WEBSITE = "http://ip-api.com/json/?fields=8217"
 HEADERS = MappingProxyType(
     {
         "User-Agent": (
-            "Mozilla/5.0 (Windows NT 10.0; rv:109.0)"
-            " Gecko/20100101 Firefox/119.0"
+            "Mozilla/5.0 (Windows NT 10.0; rv:120.0)"
+            " Gecko/20100101 Firefox/120.0"
         )
     }
 )

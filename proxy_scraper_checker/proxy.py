d45db44c6b8647f5476ce3d45c8ec3a670c1c379
monosans <hsyqixco@protonmail.com>
2023-12-07 08:08:12 +0000

    Update .pre-commit-config.yaml (#111)
    * Update .pre-commit-config.yaml
    
    * Ignore PLR0917 in ruff
    
    * Reformat
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- proxy_scraper_checker/proxy.py
+++ proxy_scraper_checker/proxy.py
@@ -13,14 +13,11 @@ from aiohttp_socks import ProxyConnector, ProxyType
 from .null_context import AsyncNullContext
 
 DEFAULT_CHECK_WEBSITE = "http://ip-api.com/json/?fields=8217"
-HEADERS = MappingProxyType(
-    {
-        "User-Agent": (
-            "Mozilla/5.0 (Windows NT 10.0; rv:120.0)"
-            " Gecko/20100101 Firefox/120.0"
-        )
-    }
-)
+HEADERS = MappingProxyType({
+    "User-Agent": (
+        "Mozilla/5.0 (Windows NT 10.0; rv:120.0) Gecko/20100101 Firefox/120.0"
+    )
+})
 
 
 @dataclass(repr=False, unsafe_hash=True)

eeca39b8f5325fd4f77ed1a7906730d63ccfe11e
monosans <hsyqixco@protonmail.com>
2023-12-26 10:09:22 +0300

    Update User-Agent

--- proxy_scraper_checker/constants.py
+++ proxy_scraper_checker/constants.py
@@ -7,6 +7,6 @@ from aiohttp import hdrs
 DEFAULT_CHECK_WEBSITE = "http://ip-api.com/json/?fields=8217"
 HEADERS: MappingProxyType[str, str] = MappingProxyType({
     hdrs.USER_AGENT: (
-        "Mozilla/5.0 (Windows NT 10.0; rv:120.0) Gecko/20100101 Firefox/120.0"
+        "Mozilla/5.0 (Windows NT 10.0; rv:121.0) Gecko/20100101 Firefox/121.0"
     )
 })

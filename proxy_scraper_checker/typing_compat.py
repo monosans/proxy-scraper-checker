e201011ed390e9ebc2e53500853dfd3129351f70
monosans <hsyqixco@protonmail.com>
2024-01-25 18:12:36 +0300

    Refactor typing_compat

--- proxy_scraper_checker/typing_compat.py
+++ proxy_scraper_checker/typing_compat.py
@@ -2,9 +2,9 @@ from __future__ import annotations
 
 import sys
 
-if sys.version_info < (3, 11):
-    from typing_extensions import Any, Literal, Self
-else:
+if sys.version_info >= (3, 11):
     from typing import Any, Literal, Self
+else:
+    from typing_extensions import Any, Literal, Self
 
 __all__ = ("Any", "Literal", "Self")

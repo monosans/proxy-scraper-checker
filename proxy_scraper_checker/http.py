624ffa2698173d5c9bbec0a3d76d888aa543dd23
monosans <hsyqixco@protonmail.com>
2024-01-29 14:04:37 +0300

    Add noqa

--- proxy_scraper_checker/http.py
+++ proxy_scraper_checker/http.py
@@ -18,7 +18,7 @@ class NoCharsetHeaderError(Exception):
 
 HEADERS: MappingProxyType[str, str] = MappingProxyType({
     hdrs.USER_AGENT: (
-        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36"
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36"  # noqa: E501
     )
 })
 

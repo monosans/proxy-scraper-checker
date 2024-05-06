fdae372a081c27587ddf00431fc10043ec19dec1
monosans <hsyqixco@protonmail.com>
2024-05-06 22:53:34 +0500

    Update User-Agent

--- proxy_scraper_checker/http.py
+++ proxy_scraper_checker/http.py
@@ -11,7 +11,7 @@ from .utils import bytes_decode
 
 HEADERS: MappingProxyType[str, str] = MappingProxyType({
     hdrs.USER_AGENT: (
-        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36"  # noqa: E501
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36"  # noqa: E501
     )
 })
 SSL_CONTEXT = ssl.create_default_context(cafile=certifi.where())

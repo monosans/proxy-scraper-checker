67fb5165368659a2175e2b196bb9cbe1145a295a
monosans <hsyqixco@protonmail.com>
2024-10-01 15:53:42 +0300

    chore: switch from poetry to uv

--- start-termux.sh
+++ start-termux.sh
@@ -3,5 +3,5 @@
 set -eu
 
 python3 -m venv --upgrade-deps .venv
-.venv/bin/python3 -m pip install -U --disable-pip-version-check --editable .[termux]
+.venv/bin/python3 -m pip install -U --disable-pip-version-check --editable .
 .venv/bin/python3 -m proxy_scraper_checker

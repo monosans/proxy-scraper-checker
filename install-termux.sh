220e3d93e672b17ae6ff969d360b60712f030a9d
monosans <hsyqixco@protonmail.com>
2024-11-28 08:51:05 +0300

    fix(termux): don't install libuv

--- install-termux.sh
+++ install-termux.sh
@@ -9,7 +9,7 @@ download_path="${TMPDIR}/${project_name}.zip"
 
 [ -d "${install_path}" ] && rm -rf --interactive=once "${install_path}"
 pkg upgrade --yes -o Dpkg::Options::='--force-confdef'
-pkg install --yes binutils libuv python python-pip
+pkg install --yes binutils python python-pip
 curl -fsSLo "${download_path}" "https://github.com/monosans/${project_name}/archive/refs/heads/main.zip"
 unzip -d "${base_path}" "${download_path}"
 rm -f "${download_path}"

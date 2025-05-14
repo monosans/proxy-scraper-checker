30a9e9aee6483761c2712b4a4a803365fd1e635f
monosans <hsyqixco@protonmail.com>
2025-05-15 01:27:32 +0300

    improve regex

--- src/parsers.rs
+++ src/parsers.rs
@@ -5,18 +5,19 @@ use color_eyre::eyre::{OptionExt as _, WrapErr as _};
 pub static PROXY_REGEX: LazyLock<fancy_regex::Regex> = LazyLock::new(
     move || {
         fancy_regex::RegexBuilder::new(
-        r"(?:^|[^\dA-Za-z])(?:(?P<protocol>https?|socks[45]):\/\/)?(?:(?P<username>[^\s:@]+):(?P<password>[^\s:@]+)@)?(?P<host>(?:[\-\.\dA-Za-z]+|(?:\d|[1-9]\d|1\d{2}|2[0-4]\d|25[0-5])(?:\.(?:\d|[1-9]\d|1\d{2}|2[0-4]\d|25[0-5])){3})):(?P<port>\d|[1-9]\d{1,3}|[1-5]\d{4}|6[0-4]\d{3}|65[0-4]\d{2}|655[0-2]\d|6553[0-5])(?=[^\dA-Za-z]|$)",
-    )
-    .case_insensitive(true)
-    .build()
-    .unwrap()
+            r"(?:^|[^0-9A-Za-z])(?:(?P<protocol>https?|socks[45]):\/\/)?(?:(?P<username>[\dA-Za-z]+):(?P<password>[\dA-Za-z]+)@)?(?P<host>[A-Za-z][\-\.A-Za-z]*[A-Za-z]|[A-Za-z]|(?:[0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])(?:\.(?:[0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])){3}):(?P<port>[0-9]|[1-9][0-9]{1,3}|[1-5][0-9]{4}|6[0-4][0-9]{3}|65[0-4][0-9]{2}|655[0-2][0-9]|6553[0-5])(?=[^0-9A-Za-z]|$)",
+        )
+        .case_insensitive(true)
+        .build()
+        .unwrap()
     },
 );
 
 static IPV4_REGEX: LazyLock<fancy_regex::Regex> = LazyLock::new(move || {
     fancy_regex::Regex::new(
-        r"^(?:[0-9:A-Fa-f]+,)?\s*(?P<host>(?:\d|[1-9]\d|1\d{2}|2[0-4]\d|25[0-5])(?:\.(?:\d|[1-9]\d|1\d{2}|2[0-4]\d|25[0-5])){3})(?::(?:\d|[1-9]\d{1,3}|[1-5]\d{4}|6[0-4]\d{3}|65[0-4]\d{2}|655[0-2]\d|6553[0-5]))?\s*$"
-    ).unwrap()
+        r"^\s*(?:[0-9:A-Fa-f]+,)?\s*(?P<host>(?:[0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])(?:\.(?:[0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])){3})(?::(?:[0-9]|[1-9][0-9]{1,3}|[1-5][0-9]{4}|6[0-4][0-9]{3}|65[0-4][0-9]{2}|655[0-2][0-9]|6553[0-5]))?\s*$"
+    )
+    .unwrap()
 });
 
 pub fn parse_ipv4(s: &str) -> color_eyre::Result<Option<String>> {

2c0ffe62c271b4b5e484617a4d4e6f0659843ed2
monosans <hsyqixco@protonmail.com>
2025-05-28 17:17:47 +0300

    remove unused import

--- src/parsers.rs
+++ src/parsers.rs
@@ -1,7 +1,5 @@
 use std::sync::LazyLock;
 
-use color_eyre::eyre::WrapErr as _;
-
 pub static PROXY_REGEX: LazyLock<fancy_regex::Regex> = LazyLock::new(|| {
     fancy_regex::RegexBuilder::new(
         r"(?:^|[^0-9A-Za-z])(?:(?P<protocol>https?|socks[45]):\/\/)?(?:(?P<username>[0-9A-Za-z]+):(?P<password>[0-9A-Za-z]+)@)?(?P<host>[A-Za-z][\-\.A-Za-z]*[A-Za-z]|[A-Za-z]|(?:[0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])(?:\.(?:[0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])){3}):(?P<port>[0-9]|[1-9][0-9]{1,3}|[1-5][0-9]{4}|6[0-4][0-9]{3}|65[0-4][0-9]{2}|655[0-2][0-9]|6553[0-5])(?=[^0-9A-Za-z]|$)",

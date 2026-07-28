3b7d813ba3e009bf1799f46b72c502ac723e0e46
monosans <hsyqixco@protonmail.com>
2026-07-29 02:52:39 +0300

    Revert "ci: fix clippy"
    This reverts commit aaf746ce452de721d9e32feffa7fe4db7eed1764.

--- src/tui.rs
+++ src/tui.rs
@@ -29,6 +29,7 @@ use crate::{
 
 pub struct RatatuiRestoreGuard;
 
+#[expect(clippy::missing_trait_methods)]
 impl Drop for RatatuiRestoreGuard {
     fn drop(&mut self) {
         ratatui::restore();

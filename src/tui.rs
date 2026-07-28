aaf746ce452de721d9e32feffa7fe4db7eed1764
monosans <hsyqixco@protonmail.com>
2026-07-28 15:52:44 +0300

    ci: fix clippy

--- src/tui.rs
+++ src/tui.rs
@@ -29,7 +29,6 @@ use crate::{
 
 pub struct RatatuiRestoreGuard;
 
-#[expect(clippy::missing_trait_methods)]
 impl Drop for RatatuiRestoreGuard {
     fn drop(&mut self) {
         ratatui::restore();
